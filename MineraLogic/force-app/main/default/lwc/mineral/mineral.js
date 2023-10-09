c67110646e72bedcbe483b23935e1162964c007a
Juan Ortiz <ortizub41@users.noreply.github.com>
2023-10-08 23:02:11 -0400

    Update mineral.js

--- MineraLogic/force-app/main/default/lwc/mineral/mineral.js
+++ MineraLogic/force-app/main/default/lwc/mineral/mineral.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { labels } from './mineralHelper.js';
 import getLocations from '@salesforce/apex/Mineral.getLocationInformation';
 
@@ -6,10 +6,10 @@ const LATITUDE = '1.6596';
 const LONGITUDE = '28.0339';
 
 export default class Mineral extends LightningElement {
+	@track mapMarkers = [];
 	listView = 'visible';
 	selectedMarkerValue = 'Min1';
 	error;
-	mapMarkers = [];
 	labels = labels;
 
 	connectedCallback() {
@@ -20,7 +20,8 @@ export default class Mineral extends LightningElement {
 				const loc = {
 					location: { Latitude: location.latitude, Longitude: location.longitude},
 					title: location.title,
-					value: location.value
+					value: location.value,
+					description: 'Hello World'
 				}
 				constLocation.push(JSON.parse(JSON.stringify(loc)));
 			});
@@ -36,5 +37,23 @@ export default class Mineral extends LightningElement {
 
 	handleMarkerSelect(event) {
 		this.selectedMarkerValue = event.target.selectedMarkerValue;
+	}
+
+	addRegion() {
+		this.template.querySelector('c-mineral-region-modal').displayModal();
+	}
+
+	updateRegionView(event) {
+		const constLocation = [];
+		const loc = {
+			location: {
+				Latitude: event.detail.latitude,
+				Longitude: event.detail.longitude
+			},
+			title: event.detail.title,
+			value: ''
+		}
+		constLocation.push(JSON.parse(JSON.stringify(loc)));
+		this.mapMarkers = [...this.mapMarkers, ...constLocation]
 	}
 }
